0fadc871b16860d0658031ad93eb48cd1eeb0ef0
Dan Gautsch <dgautsch@gmail.com>
2018-03-31 17:47:22 -0400

    Formatting file

--- src/tweet.py
+++ src/tweet.py
@@ -11,9 +11,9 @@ class Tweet(object):
         super(Tweet, self).__init__()
         self.config = BaseConfig()
         self.api = twitter.Api(self.config.TWT_CONSUMER_KEY,
-                          self.config.TWT_CONSUMER_SECRET,
-                          self.config.TWT_ACCESS_TOKEN,
-                          self.config.TWT_ACCESS_SECRET)
+                               self.config.TWT_CONSUMER_SECRET,
+                               self.config.TWT_ACCESS_TOKEN,
+                               self.config.TWT_ACCESS_SECRET)
 
     def post(self, message):
         print('Posting tweet' + message)
