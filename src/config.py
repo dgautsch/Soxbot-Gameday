30c3a9d38ff28fe5ba67bce577024054b3e52055
Dan Gautsch <dgautsch@gmail.com>
2018-04-01 13:18:26 -0400

    PEP8 formatting and removing header for ALC rows, cleaning up old comments.

--- src/config.py
+++ src/config.py
@@ -2,8 +2,11 @@
 
 import os
 import simplejson as json
+
+
 class Config(object):
     """Environment Configuration."""
+
     def __init__(self):
         super(Config, self).__init__()
         self.CWD = os.path.dirname(os.path.realpath(__file__))
@@ -17,16 +20,25 @@ class Config(object):
         self.TWT_ACCESS_TOKEN = os.environ.get('TWT_ACCESS_TOKEN')
         self.TWT_ACCESS_SECRET = os.environ.get('TWT_ACCESS_SECRET')
         with open(self.CWD + '/settings.json') as data:
-			settings = json.load(data)
-			self.REDIRECT_URI = settings.get('REDIRECT_URI')
+            settings = json.load(data)
+            self.REDIRECT_URI = settings.get('REDIRECT_URI')
 
     def checkAuthSettings(self):
-        if self.CLIENT_ID == None: return "Missing CLIENT_ID"
-        if self.CLIENT_SECRET == None: return "Missing CLIENT_SECRET"
-        if self.REDIRECT_URI == None: return "Missing REDIRECT_URI"
-        if self.REFRESH_TOKEN == None: return "Missing REFRESH_TOKEN"
-        if self.SUBREDDIT == None: return "Missing SUBREDDIT"
-        if self.TWT_CONSUMER_KEY == None: return "Missing TWT_CONSUMER_KEY"
-        if self.TWT_CONSUMER_SECRET == None: return "Missing TWT_CONSUMER_SECRET"
-        if self.TWT_ACCESS_TOKEN == None: return "Missing TWT_ACCESS_TOKEN"
-        if self.TWT_ACCESS_SECRET == None: return "Missing TWT_ACCESS_SECRET"
+        if self.CLIENT_ID is None:
+            return "Missing CLIENT_ID"
+        if self.CLIENT_SECRET is None:
+            return "Missing CLIENT_SECRET"
+        if self.REDIRECT_URI is None:
+            return "Missing REDIRECT_URI"
+        if self.REFRESH_TOKEN is None:
+            return "Missing REFRESH_TOKEN"
+        if self.SUBREDDIT is None:
+            return "Missing SUBREDDIT"
+        if self.TWT_CONSUMER_KEY is None:
+            return "Missing TWT_CONSUMER_KEY"
+        if self.TWT_CONSUMER_SECRET is None:
+            return "Missing TWT_CONSUMER_SECRET"
+        if self.TWT_ACCESS_TOKEN is None:
+            return "Missing TWT_ACCESS_TOKEN"
+        if self.TWT_ACCESS_SECRET is None:
+            return "Missing TWT_ACCESS_SECRET"
